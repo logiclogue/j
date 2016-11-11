2b62901d568d5f020b6200cdbc1a284a653c0ce6
logiclogue <jordanpaullord@hotmail.co.uk>
2016-11-11 12:53:08 +0000

    Updates Model to mongoose model

--- src/weight/Model.js
+++ src/weight/Model.js
@@ -1,41 +1,14 @@
-var Backbone = require('backbone');
+var Mongoose = require('mongoose');
+var Schema = Mongoose.Schema;
 
-class Model {
-    
-    constructor() {
-        this.date = "";
-        this.weight = 00.0;
+var Model = new Schema({
+    date: {
+        type: String,
+        default: ""
+    },
+    weight: {
+        type: Number
     }
-
-    validate() {
-        var errors = [];
-
-        if (typeof this.weight !== "number")
-        {
-            errors.push("Weight is not a number");
-        }
-
-        if (typeof this.date !== "string")
-        {
-            errors.push("Date is not a string");
-        }
-
-        if (this.date.length !== 10)
-        {
-            errors.push("Date is not correct format");
-        }
-
-        return errors;
-    }
-
-    get isValid()
-    {
-        var errors = this.validate();
-        var errorCount = errors.length;
-
-        return errorCount === 0;
-    }
-
-}
+});
 
 module.exports = Model;
